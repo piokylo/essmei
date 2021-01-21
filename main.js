acb233dc6b6613c861637e6a6c64c132c9512725
Sean Kiely <seankiely670@gmail.com>
2021-01-21 21:27:55 +0000

    did thing

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.filters').on('click', function(){
+
+
+    if($(".filters-list").css("display") == "none"){
+$('.filters-list').css("display", "table")
+    } else {
+        $('.filters-list').css("display", "none")
+
+    }
+
+
+    
+})
+
