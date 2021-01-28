ecf27a0552ada7945c89df52993ce820e4d8bf9c
Sean Kiely <seankiely670@gmail.com>
2021-01-28 13:26:36 +0000

    Added Filters

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
-$('.filters').on('click', function(){
+let links = $('.filters, .filters-list');
+
+links.click(function (e) {
+    e.preventDefault();
+});
 
 
-    if($(".filters-list").css("display") == "none"){
-$('.filters-list').css("display", "table")
+$('.show-filters').on('click', function () {
+
+  
+
+
+    if ($('.filters-list').is(':visible')){
+
+        $('.filters-list').slideUp()
+        $('.show-filters').text('Show Filters')
+
     } else {
-        $('.filters-list').css("display", "none")
+        $('.filters-list').slideDown()
+        $('.show-filters').text('Hide Filters')
 
     }
 
 
+
+   
     
+  
+   
+})
+
+
+
+$('.filters-list a').on('click', function () {
+
+
+    let filter = $(this).data('filter');
+
+    
+    $('.product').hide();
+    $(filter).show();
+    $('.filters-list a').removeClass('selected')
+    $(this).addClass('selected')
+   
+
+
 })
 
